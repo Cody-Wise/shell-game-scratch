2979234dfadf1d1d6ecb98a9170edae03b61ebde
Cody-Wise <cody@sharptackmedia.com>
2022-03-08 10:55:21 -0800

    Grabbed dom elements

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 // import functions and grab DOM elements
 
+const buttonOneEl = document.getElementById('shell-one-button');
+const buttonTwoEl = document.getElementById('shell-two-button');
+const buttonThreeEl = document.getElementById('shell-three-button');
+const imageOneEl = document.getElementById('img1');
+const imageTwoEl = document.getElementById('img2');
+const imageThreeEl = document.getElementById('img3');
+const winsEl = document.getElementById('wins');
+const lossesEl = document.getElementById('losses');
+const totalEl = document.getElementById('total');
+
+
+
 // let state
+
+
 
 // set event listeners 
   // get user input
